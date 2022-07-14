7a6068aa1096d3bd91791030d41e896944580386
fruixs <kiminoegaoarigatou@gmail.com>
2022-07-15 00:04:35 +0900

    README update

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # 과제 및 기타 저장소
 
-* [우리동작 오픈소스 프로그래밍 03회차](./WRDJClass/03강.html)
+* [우리동작 오픈소스 프로그래밍 03회차](./example/WRDJClass/03강.html)
 * [회원 가입 양식 만들기](./example/01R.html)
-* [for 문으로 table 만들기](./WRDJClass/create-table-for.html)
-* [우리동작 오픈소스 프로그래밍 05회차](./WRDJClass/05강.html)
+* [for 문으로 table 만들기](./example/WRDJClass/create-table-for.html)
+* [우리동작 오픈소스 프로그래밍 05회차](./example/WRDJClass/05강.html)
 * [복잡한 표 접근성 적용하기](./example/complex-table.html)
 * [CSS float 속성을 활용한 메뉴 만들기](./example/float-menu.html)
 * [웹표준 수업 김일규 07, 06, 2022](./example/KIGClass/이력서.html)
+* [javascript cuncurrency problem 1](./example/example-concurrency-problem/concurrency-problem.html)
---
+++ example/WRDJClass/WRDJClass03.js
@@ -0,0 +1,71 @@
+// 'use strict'
+(function(usrbtn){
+    if(usrbtn === undefined) {
+        window.alert("버튼 아이디를 제대로 입력하세요.");
+        return;
+    }
+    else { 
+    usrbtn.addEventListener("click",function(event) {
+        // console.log(event.target.id);
+        let limitNum = 0;
+        function usrfunc(limitNum){
+            if(limitNum < 3) {
+                let usrprompt = (event.target.id === "usrbtn")? prompt("4 글자 이상 입력하세요."):null;
+                if (usrprompt === null) {
+                    window.alert(limitNum + " 번재 : 입력 좀 하시지요.")
+                    ++limitNum;
+                    usrfunc(limitNum);
+                } else if(usrprompt.length >= 4) {
+                    window.alert("4글자 이상 입력하였습니다.")
+                    return;
+                } else {
+                    window.alert(limitNum + " 번재 : 4글자 이하 입력하였습니다.")
+                    ++limitNum;
+                    usrfunc(limitNum);
+                }
+            } else {
+                document.location.href="http://warning.or.kr/"
+                return;
+            }
+        }
+        usrfunc(limitNum);
+    })}
+      
+})(
+    (function(){
+        try {
+            // try statement 는 parse 단계 컴파일 애러는 처리하지 못 한다.
+            // 괄호가 짝이 맞지 않아 생긴 애러는 예외없이 출력한다는 얘기임
+            if(document.querySelector("#usrbtn") === null) {
+                return undefined;
+            } else {
+                return document.querySelector("#usrbtn");
+            }
+        }
+        catch(error) {
+            window.alert("버튼 아이디: " + error.message);
+            window.alert("버튼의 아이디를 바르게 입력하세요.")
+        }
+    })()
+)
+
+/* 
+function usrevt(event) {
+    if( this === undefined) {
+        console.log('use strict mode')
+        
+        let usrprompt = (event.target.className === 'usrbtn')? window.prompt("글자를 입력하세요."):null;
+        if(usrprompt === null) {
+            window.alert("사용자가 입력을 취소하였습니다.")
+        } 
+        else if(usrprompt.length >= 4) {
+            window.alert("4 글자 이상 입력하였습니다.");
+        }
+        else {
+            window.alert("4 글자 이하 입력하였습니다.");
+        }
+
+    } else {
+        
+    }
+} */
