81f41aed958c74d45c4b720721ead00fe695804e
= <=>
2022-11-24 16:17:00 +0900

    aria 스크립트 적용

--- NCWClass/asset/js/index.js
+++ NCWClass/asset/js/index.js
@@ -1,12 +1,25 @@
 function menu(id) {
     let target = document.getElementById(id)
-    if(getComputedStyle(target).display === 'none') {
-        target.style.display='block'
-        target.ariaHidden=false
-        target.ariaExpanded='true'
+    if (getComputedStyle(target).display === 'none') {
+        target.style.display = 'block'
+        target.ariaHidden = false
+        target.ariaExpanded = 'true'
     } else {
-        target.style.display='none'
-        target.ariaHidden=true
-        target.ariaExpanded='false'
+        target.style.display = 'none'
+        target.ariaHidden = true
+        target.ariaExpanded = 'false'
     }
 }
+document.querySelector("nav > ul > li:nth-child(1) > button").addEventListener('keyup', function (e) {
+    if (e.key === 'ArrowDown') {
+        console.log(this)
+        this.nextElementSibling.children[0].children[0].focus()
+    }
+})
+
+document.querySelector("nav > ul > li:nth-child(2) > button").addEventListener('keyup', function (e) {
+    if (e.key === 'ArrowDown' && document.activeElement === this) {
+        console.log(this)
+        this.nextElementSibling.children[0].children[0].focus()
+    }
+})
